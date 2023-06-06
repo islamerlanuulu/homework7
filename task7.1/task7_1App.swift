614890264f12ff577c9d4484e93d0047d76e9bad
islamerlanuulu <islamerlanuulu3939@gmail.com>
2023-06-06 18:28:19 +0600

    Initial Commit

---
+++ task7.1/task7_1App.swift
@@ -0,0 +1,17 @@
+//
+//  task7_1App.swift
+//  task7.1
+//
+//  Created by Islam Erlan Uulu on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct task7_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
